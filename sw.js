a9b3a639cb8ee69fba7771bb0dc2fce5c18d81a9
tim <tim@prestige-inc.jp>
2023-11-17 15:43:50 +0900

    up

--- sw.js
+++ sw.js
@@ -1,16 +1,11 @@
-const CACHE_NAME = 'v0.0.7';
+const CACHE_NAME = 'v0.0.9';
 const urlsToCache = [
     '/',
     "/index.html",
     "/css/style.css",
     "/js/app.js",
     "/manifest.json",
-
-
-
 ];
-
-
 
 // sw.js
 self.addEventListener('install', event => {
@@ -20,12 +15,13 @@ self.addEventListener('install', event => {
         return cache.addAll(urlsToCache);
       })
     );
-  });
+});
   
 
 self.addEventListener('fetch', event => {
     event.respondWith(
       caches.match(event.request).then(response => {
+        console.log(response)
         return response || fetch(event.request);
       })
     )
